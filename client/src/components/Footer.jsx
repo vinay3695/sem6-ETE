60ac058ce03dcef275b88d068be2ef585b2a552b
Shivam Gulati <shivam3692.be20@chitkara.edu.in>
2023-06-07 18:29:32 +0530

    firsty commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../img/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src="https://www.liblogo.com/img-logo/me2070mbed-medium-logo-medium-icon-free-download-on-iconfinder.png" alt="" />
+      <span>
+        Made as <b>Meduim clone</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
